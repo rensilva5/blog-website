9047f69813ae822a7120ccd7062facfdc335c4a5
rensilva5 <renzoasilva@gmail.com>
2023-05-01 14:57:36 -0400

    Updates

---
+++ precache-manifest.7a99f4802cb8e1d007f19d9648bdc1f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "128b7c2f57c8fbcde495",
+    "url": "//github.com/rensilva5/blog-website/static/css/main.ef8a8e38.chunk.css"
+  },
+  {
+    "revision": "128b7c2f57c8fbcde495",
+    "url": "//github.com/rensilva5/blog-website/static/js/main.128b7c2f.chunk.js"
+  },
+  {
+    "revision": "7ee3e3452993bcfe7eaf",
+    "url": "//github.com/rensilva5/blog-website/static/js/1.7ee3e345.chunk.js"
+  },
+  {
+    "revision": "e07666b7a8ffcbbc52dd",
+    "url": "//github.com/rensilva5/blog-website/static/js/runtime~main.e07666b7.js"
+  },
+  {
+    "revision": "0942dbde0e194bb15509f15059b8c35d",
+    "url": "//github.com/rensilva5/blog-website/index.html"
+  }
+];
